cd3ca12d5d9e7e4d6c41bc3f6e342c3e7d16c950
maucaruso <carusojr@outlook.com>
2022-09-25 15:39:29 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
+export { categoriesRoutes };
