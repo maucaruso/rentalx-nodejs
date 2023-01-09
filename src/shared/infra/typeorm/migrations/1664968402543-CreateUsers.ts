564a66d3a4d9d6d3df341386472fd5ddd3d1c32d
maucaruso <carusojr@outlook.com>
2023-01-09 07:03:42 -0300

    feat: create rentals migration

--- src/shared/infra/typeorm/migrations/1664968402543-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1664968402543-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1664968402543 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
