3dc39a22ec75c51e543af96f3f0182fd5f179601
maucaruso <carusojr@outlook.com>
2023-01-24 08:28:13 -0300

    feat: implementing refresh token to auth route

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "ac5f0b04d2c3875062604898b085bdb5",
+  expires_in_token: "15m",
+  secret_refresh_token: "48891d9948a3add30faff76e6c0705b5",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
