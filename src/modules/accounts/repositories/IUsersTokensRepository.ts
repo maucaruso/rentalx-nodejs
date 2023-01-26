e671df9e233ac33a6d93c637665de6248163f6e5
maucaruso <carusojr@outlook.com>
2023-01-26 08:15:03 -0300

    feat: password recovery route

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -13,6 +13,8 @@ interface IUsersTokensRepository {
     refresh_token: string
   ): Promise<UserTokens>;
 
+  findByRefreshToken(refresh_token: string): Promise<UserTokens>;
+
   deleteById(id: string): Promise<void>;
 }
 
