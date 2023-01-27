c7a4400556c8a2fedeca5457e8eb0d965bc73d9e
maucaruso <carusojr@outlook.com>
2023-01-27 08:15:02 -0300

    feat: adjusting tests

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,53 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  usersTokens: UserTokens[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+
+    Object.assign(userToken, {
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    this.usersTokens.push(userToken);
+    return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    const userToken = this.usersTokens.find(
+      (userToken) =>
+        userToken.user_id === user_id &&
+        userToken.refresh_token === refresh_token
+    );
+
+    return userToken;
+  }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = this.usersTokens.find(
+      (userToken) => userToken.refresh_token === refresh_token
+    );
+
+    return userToken;
+  }
+
+  async deleteById(id: string): Promise<void> {
+    const userToken = this.usersTokens.find((user) => user.id === id);
+    this.usersTokens.splice(this.usersTokens.indexOf(userToken));
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -34,7 +34,7 @@ describe("List Categories", () => {
       password: "admin",
     });
 
-    const { token } = responseToken.body;
+    const { refresh_token } = responseToken.body;
 
     await request(app)
       .post("/categories")
@@ -43,7 +43,7 @@ describe("List Categories", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${refresh_token}`,
       });
 
     const response = await request(app).get("/categories");
